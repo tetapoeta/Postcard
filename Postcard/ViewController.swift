d2f6cf072734817fbbc9425218186eac8e93016c
Alvaro Imperial <alvaro.imperial@gmail.com>
2014-09-10 19:34:27 +0200

    nameLabelFuncionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var messageLabel : UILabel
+    @IBOutlet var nameLabel : UILabel
+    
+    @IBOutlet var enterNameTextField : UITextField
+    @IBOutlet var enterMessageTextField : UITextField
+    
+    @IBOutlet var mailButton : UIButton
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender : UIButton)
+    {
+        // Code will evaluate when we press the button
+        messageLabel.hidden = false
+        messageLabel.text  = enterMessageTextField.text
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        nameLabel.textColor = UIColor.blueColor()
+        
+        mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+        
+        
+        
+    }
 
 }
 
